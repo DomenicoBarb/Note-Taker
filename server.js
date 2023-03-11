d63438101e6e681ed6fa6a259725e33d39b69f0a
DomenicoBarb <domenicobarberi585@gmail.com>
2023-03-11 14:23:03 -0500

    heroku fix

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // Var declaration/dependencies
 const express = require('express');
-const apiRoutes = require('./routes/apiRoutes/index');
-const htmlRoutes = require('./routes/htmlRoutes/index');
+const apiRoutes = require('./routes/apiRoutes/index.js');
+const htmlRoutes = require('./routes/htmlRoutes/index.js');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
