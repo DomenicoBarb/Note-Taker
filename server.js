74816135354f09d532bc52892603f7f6891c1366
DomenicoBarb <domenicobarberi585@gmail.com>
2023-03-11 13:48:22 -0500

    server.js complete, htmlroutes/index complete

---
+++ server.js
@@ -0,0 +1,23 @@
+// Var declaration/dependencies
+const express = require('express');
+const apiRoutes = require('./routes/apiRoutes/index');
+const htmlRoutes = require('./routes/htmlRoutes/index');
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// This line adds middleware to parse incoming request bodies with urlencoded payloads
+app.use(express.urlencoded({ extended: true }));
+// This line adds middleware to parse incoming request bodies with JSON payloads
+app.use(express.json());
+// This line serves static files from the 'public' directory in the root of the application
+app.use(express.static('public'));
+
+// Use apiRoutes
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+// This line starts the Express.js server and listens on a specified port (PORT)
+app.listen(PORT, () => {
+    // When the server starts, this callback function is called and logs a message to the console
+    console.log(`API server now on port ${PORT}!`);
+});
