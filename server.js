01b1332d7f5ffaec88b61cdbab7ec9a66503c9b3
DomenicoBarb <domenicobarberi585@gmail.com>
2023-03-11 15:58:46 -0500

    test commit

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // Var declaration/dependencies!
-const apiRoutes = require('./routes/apiRoutes/index.js');
 const htmlRoutes = require('./routes/htmlRoutes/index.js');
+const apiRoutes = require('./routes/apiRoutes/index.js');
 
 const express = require('express');
 
