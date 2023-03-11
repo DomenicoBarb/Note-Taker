9e83d476746c338ec90e07c957daa94c0cb4fa4f
DomenicoBarb <domenicobarberi585@gmail.com>
2023-03-11 15:38:01 -0500

    corrected name to path

--- routes/apiroutes/index.js
+++ routes/apiroutes/index.js
@@ -1,7 +1,7 @@
 // Import the Express library and create a router object
 const router = require('express').Router();
 // Import the noteRoutes module
-const noteRoutes = require('../apiRoutes/noteRoutes');
+const noteRoutes = require('../apiRoutes/routes');
 
 // Use the noteRoutes module for all routes on this router
 router.use(noteRoutes);
