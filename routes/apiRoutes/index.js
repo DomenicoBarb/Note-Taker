825cf02a878abe33f204b7c08637e0629e912e22
DomenicoBarb <domenicobarberi585@gmail.com>
2023-03-11 16:14:03 -0500

    removed test route.js and reverted to previous ver of apiRoutes/index.js as the error was caused by apiRoutes/htmlRoutes not following camel case on repo but was locally, causing inability to locate file

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,10 +1,41 @@
-// Import the Express library and create a router object
+// Var declaration/dependencies
 const router = require('express').Router();
-// Import the noteRoutes module
-const noteRoutes = require('../apiRoutes/routes');
+const fs = require('fs');
 
-// Use the noteRoutes module for all routes on this router
-router.use(noteRoutes);
+// Reads the database file and return the saved notes as a json
+router.get('/notes', (req, res) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        const notes = JSON.parse(data);
+        res.json(notes);
+    });
+});
 
-// Export the router object for use in other modules
+// Post request receives new note to save on the request body, add it to the database file, and return the new note to the client
+router.post('/notes', (req, res) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        const notes = JSON.parse(data);
+        const note = req.body;
+        const id = (notes.length).toString();
+        note.id = id;
+        notes.push(note);
+
+        fs.writeFileSync('db/db.json', JSON.stringify(notes));
+
+        res.json(notes);
+    });
+});
+
+// Deletes a selected note based on id(deleted starter sample note, it had no id and could not be deleted)(bonus)
+router.delete('/notes/:id', (req, res) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        const notes = JSON.parse(data);
+        const id = req.params.id;
+        const deleted = notes.filter((target) => target.id != id);
+
+        fs.writeFileSync('./db/db.json', JSON.stringify(deleted));
+
+        res.json(notes);
+    });
+});
+
 module.exports = router;
